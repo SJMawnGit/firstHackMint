867106fc10c51a52f7d41b168ffbe144df02d2d7
SJMawn <SJmawn@gmail.com>
2020-06-29 15:25:15 -0400

    titles displaying

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import logo from './mintbean.png';
+import algoliasearch from 'algoliasearch'
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Pagination,
+  Highlight,
+  ClearRefinements,
+  RefinementList,
+  Configure,
+} from 'react-instantsearch-dom';
+import PropTypes from 'prop-types';
 import './styles/index.css';
+
+const searchClient = algoliasearch('OEELBRLG33','8e0eb8dd3c6e31c8b34141178ad4f89a')
 
 function App() {
   return (
     <div className="App">
+      <h1>big search type stuff</h1>
+      <InstantSearch indexName = "vgSales" searchClient = {searchClient}>
+        <div>
+          <SearchBox />
+          <Hits hitComponent = {Hit}/>
+        </div>
+      </InstantSearch>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>App.js</code> is the entrypoint to your app.
-        </p>
         <h2>HAPPY HACKING!</h2>
         <a
           className="App-link"
@@ -21,5 +39,20 @@ function App() {
     </div>
   );
 }
+function Hit(props) {
+  console.log(props.hit.Name)
+  return (
+    <div>
+      <div className="hit-name">
+        <Highlight attribute="name" hit={props.hit} />
+      </div>
+      <div className="hit-price">{props.hit.Name}</div>
+    </div>
+  );
+
+}
+Hit.propTypes = {
+  hit: PropTypes.object.isRequired,
+};
 
 export default App;
