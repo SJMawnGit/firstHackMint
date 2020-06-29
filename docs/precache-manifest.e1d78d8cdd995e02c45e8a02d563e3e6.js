ee2bb6165c8a8b0bfa6ec66834a0c3e940c14bcf
SJMawn <SJmawn@gmail.com>
2020-06-29 14:43:05 -0400

    renamed to docs

---
+++ docs/precache-manifest.e1d78d8cdd995e02c45e8a02d563e3e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "285cdaf425b4547e45d59b3cf1a342dd",
+    "url": "/firstHackMint/index.html"
+  },
+  {
+    "revision": "a2d4a84501105cea872b",
+    "url": "/firstHackMint/static/css/main.75df9f48.chunk.css"
+  },
+  {
+    "revision": "509bc84f223ca9790ee0",
+    "url": "/firstHackMint/static/js/2.8e412f95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/firstHackMint/static/js/2.8e412f95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d4a84501105cea872b",
+    "url": "/firstHackMint/static/js/main.a6f57e64.chunk.js"
+  },
+  {
+    "revision": "cdc34f2e27d790f8e296",
+    "url": "/firstHackMint/static/js/runtime-main.aa5d7751.js"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "/firstHackMint/static/media/mintbean.cf18b629.png"
+  }
+]);
